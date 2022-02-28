652607c564210e3c754bb52b531df5d4612a72b8
xwarheadx <sergeypost89@gmail.com>
2022-03-01 00:08:09 +0300

    Задание 3

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,31 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+type AppProps = {
+  genres: string[],
+  films: Array<{
+    name: string,
+    srcImg: string,
+  }>,
+  selectedFilm: {
+    title: string,
+    genre: string,
+    year: number,
+    srcPoster: string,
+  },
+};
+
+function App({
+  genres,
+  films,
+  selectedFilm,
+}: AppProps): JSX.Element {
+  return (
+    <Main
+      genres={genres}
+      films={films}
+      selectedFilm={selectedFilm}
+    />
+  );
 }
 
 export default App;
